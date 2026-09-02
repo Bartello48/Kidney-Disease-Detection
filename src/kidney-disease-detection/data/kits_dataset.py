de14863163c90ce308241d22c1be75d2cc56cc4d
Bartosz Bróździński <01187212@pw.edu.pl>
2026-09-02 19:56:59 +0200

    started work on sophisticated dataset trnasformation

--- src/kidney-disease-detection/data/kits_dataset.py
+++ src/kidney-disease-detection/data/kits_dataset.py
@@ -1,21 +1,46 @@
-import sys
+from pathlib import Path
 
 import torch
-from torch.utils.data import Dataset, DataLoader
-import torchvision
+from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 
 import pandas as pd
 import numpy as np
 
-print(f"system version: {sys.version}")
-print(f"PyTorch version: {torch.__version__}")
-print(f"Torchvision version: {torchvision.__version__}")
+import nibabel as nib
 
 
 class Kits23Dataset(Dataset):
-    def __init__(self, data_path, transform=None):
-        self.data = ImageFolder(data_path, transform=transform)
+    def __init__(
+        self,
+        data_path,
+        transform=None,
+        crop_margin=20,
+        hu_min=-200,
+        hu_max=300
+    ):
+        # self.data = ImageFolder(data_path, transform=transform)
+        self._root_dir = Path(data_path)
+        self._transform = transform
+        self._crop_margin = crop_margin
+        self._hu_min = hu_min
+        self._hu_max = hu_max
+
+        self._build_index()
+
+    def _build_index(self):
+        directories = sorted(self._root_dir.glob("case_*"))
+
+        for case in directories:
+            image_path = case / "imaging.nii.gz"
+            mask_path = case / "segmentation.nii.gz"
+
+            if not image_path.exists() or not mask_path.exists():
+                continue
+
+            mask = nib.load(mask_path).get_fdata()
+
+
 
     def __len__(self):
         return len(self.data)
